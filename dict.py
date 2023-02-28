56938dfebb1e1e6a6e437886d06b065db20af040
Abhyuday Rai <abhyudayrai03@gmail.com>
2023-02-28 08:48:52 +0530

    dict form, crop tables

---
+++ dict.py
@@ -0,0 +1,14 @@
+# three dictionaries:
+# counters divided into 2 parts:
+# tables divided into 3 images: 
+
+# 1st dict:
+img_name_nickname_preference = {"hots_1":["Hotspot Entry Side", "Hotspot"], "hots_2":["Hotspot Middle Side", "Hotspot"], "hots_3":["Hotspot Counter Side", "Hotspot"], "host_1":["Hostel Entry Side", "Hostel"], "host_2":["Hostel Middle Side", "Hostel"], "host_3":["Hostel Counter Side", "Hostel"], "foot_1":["Football Entry Side", "Football"], "foot_2":["Football Middle Side", "Football"], "foot_3":["Football Counter Side", "Football"], "newmessup_1":["InsideStairs Counter Side", "InsideStairs"], "newmessup_2":["InsideStairs Middle Side", "InsideStairs"], "newmessup_3":["InsideStairs TV Side", "InsideStairs"]}
+img_name_count = {"hots_1":[60, 0], "hots_2":[100, 0], "hots_3":[100, 0], "host_1":[50, 0], "host_2":[50, 0], "host_3":[50, 0], "foot_1":[50, 0], "foot_2":[50, 0], "foot_3":[50, 0], "newmessup_1":[30, 0], "newmessup_2":[30, 0], "newmessup_3":[30, 0]}
+# counter_name_count = {}
+
+image_names = ["hots.jpeg", "host.jpeg", "foot.jpeg", "newmessup.jpeg"]
+
+# img_name_count.["host_1"] = 
+print(img_name_count.get("host_1")[1])
+# def take_and_crop_image(list_of_images):
--- testpy.py
+++ testpy.py
@@ -15,7 +15,7 @@ def object_detection_with_bounding_boxes(filename, model="yolov4-tiny", confiden
     bbox, label, conf = cv.detect_common_objects(img, confidence=confidence, model=model)
      
     # Print current image's filename
-    print(f"========================\nImage processed: {filename}\n")
+    # print(f"========================\nImage processed: {filename}\n")
      
 
     count = 0
@@ -25,11 +25,11 @@ def object_detection_with_bounding_boxes(filename, model="yolov4-tiny", confiden
 
     # print(count)
     return count
+    # print( count)
 
     # Create a new image that includes the bounding boxes
     # output_image = draw_bbox(img, bbox, label, conf)
-     
-    # cv2.imshow('',output_image)
+    # cv2.imshow('', output_image)
     # cv2.waitKey(0)
 
-object_detection_with_bounding_boxes("pp1.jpeg")
+# object_detection_with_bounding_boxes(r"C:\Users\Lenovo\OneDrive\Desktop\EatEasyProject\broken_images\crop_2.jpg")

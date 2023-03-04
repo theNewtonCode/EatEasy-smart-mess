b2cd538e13218684e9ba3ae621464215887e4329
Abhyuday Rai <abhyudayrai03@gmail.com>
2023-03-04 18:45:28 +0530

    session, userdb, and login/out features created

---
+++ flask/app.py
@@ -0,0 +1,124 @@
+from flask import Flask,render_template, session, redirect, url_for
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'eateasysmart2023'
+
+@app.route("/")
+def index(name=None):
+    if 'username' in session:
+        return render_template('index.html', name=session['username'])
+    else:
+        return redirect(url_for('loginpage'))
+
+@app.route("/about")
+def about(name=None):
+    if 'username' in session:
+        return render_template('about.html', name=session['username'])
+    else:
+        return redirect(url_for('loginpage'))
+@app.route("/select")
+def select(name=None):
+    if 'username' in session:
+        return render_template('select.html', name=session['username'])
+    else:
+        return redirect(url_for('loginpage'))
+
+@app.route("/help")
+def help(name=None):
+    if 'username' in session:
+        return render_template('help.html', name=session['username'])
+    else:
+        return redirect(url_for('loginpage'))
+
+@app.route("/map")
+def map(name=None):
+    if 'username' in session:
+        return render_template('map.html', name=session['username'])
+    else:
+        return redirect(url_for('loginpage'))
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///eateasydb.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class userdata(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(12), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer, nullable=False, default=0)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/loginpage", methods=['GET', 'POST'])
+def loginpage():
+    # If the user is already logged in, redirect to the index page
+    if 'username' in session:
+        return redirect(url_for('index'))
+    log_in = False
+    data = {}
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        remember = request.form.get('remember')
+        user = userdata.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            log_in = True
+            session['username'] = username
+        else:
+            print("wrong credentials")
+                
+        data = {"username": username, "password": password, "log_in": log_in}
+    return render_template('login.html', data=data)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    signup = True
+    data = {}
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        save_password = request.form.get('save_password')
+        if not (10 <= len(username) <= 12):
+            signup = False
+        elif userdata.query.filter_by(username=username).first():
+            signup = False
+        else:
+            password_hash = generate_password_hash(password)
+            user = userdata(username=username, password=password_hash)
+            if save_password:
+                user.password = password
+            db.session.add(user)
+            db.session.commit()
+            data = {"username": username, "signup":signup}
+            return render_template('login.html', data=data)
+    return render_template('signup.html', data=data)
+
+@app.route("/userfeedback", methods=['GET', 'POST'])
+def userfeedback():
+    # If the user is not logged in, redirect to the login page
+    if 'username' not in session:
+        return redirect(url_for('loginpage'))
+    rating = None
+    if request.method == 'POST':
+        rating = request.form['rating']
+        # Update the user's rating in the database
+        user = userdata.query.filter_by(username=session['username']).first()
+        user.rating = int(rating)
+        db.session.commit()
+    return render_template('userfeedback.html', rating=rating)
+
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('loginpage'))
+
+
+if __name__ == "__main__":
+    app.run(debug =True)
--- image_process.py
+++ image_process.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 from testpy import object_detection_with_bounding_boxes as count_people
-# Define a function that counts the number of people in an image
+# Define a function that counts  the number of people in an image
 
 img_name_nickname_preference = {"hots_1":["Hotspot Entry Side", "Hotspot"], "hots_2":["Hotspot Middle Side", "Hotspot"], "hots_3":["Hotspot Counter Side", "Hotspot"], "host_1":["Hostel Entry Side", "Hostel"], "host_2":["Hostel Middle Side", "Hostel"], "host_3":["Hostel Counter Side", "Hostel"], "foot_1":["Football Entry Side", "Football"], "foot_2":["Football Middle Side", "Football"], "foot_3":["Football Counter Side", "Football"], "newmessup_1":["InsideStairs Counter Side", "InsideStairs"], "newmessup_2":["InsideStairs Middle Side", "InsideStairs"], "newmessup_3":["InsideStairs TV Side", "InsideStairs"]}
 img_name_count = {"hots_1":[60, 0], "hots_2":[100, 0], "hots_3":[100, 0], "host_1":[50, 0], "host_2":[50, 0], "host_3":[50, 0], "foot_1":[50, 0], "foot_2":[50, 0], "foot_3":[50, 0], "newmessup_1":[30, 0], "newmessup_2":[30, 0], "newmessup_3":[30, 0]}

e967f97aa8fdb954e6e24b2412d335fd49cca902
Vanshagra Rai <vanshagrarai@gmail.com>
2023-03-04 19:09:58 +0530

    Added the android cam facility

--- cctv.py
+++ cctv.py
@@ -1,41 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
-from PIL import Image
+import cv2
+import numpy as np
+import imutils
 
-# Setting the URL of the CCTV camera's web interface
-url = "http://example.com/camera"
+# Getting Image and store it
+def get_image(url):
+    # Setting the URL of the CCTV camera's web interface(Generic Case-Later Delete it!)
+    url = "http://XX.XX.XX.XXX:8080/shot.jpg"
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+    img = cv2.imdecode(img_arr, -1)
+    img = imutils.resize(img, width=1000, height=1800)
+    
+    # Storing the image in Caputres Folder as Android_cam.jpg
+    cv2.imwrite("EatEasy-smart-mess/captures/Android_cam.jpg", img)
 
-# Setting the login credentials
-username = "username"
-password = "password"
-
-# Making a GET request to the login page
-login_url = url + "/login"
-session = requests.Session()
-login_page = session.get(login_url)
-
-# Parse the HTML response with BeautifulSoup
-soup = BeautifulSoup(login_page.text, 'html.parser')
-form = soup.find('form')
-csrf_token = form.find('input', {'name': '_csrf'})['value']
-
-# Setting the login data and making a POST request to the login page
-login_data = {'username': username, 'password': password, '_csrf': csrf_token}
-session.post(login_url, data=login_data)
-
-# Making a GET request to the live view page
-live_view_url = url + "/liveview"
-live_view_page = session.get(live_view_url)
-
-# Parse the HTML response with BeautifulSoup
-soup = BeautifulSoup(live_view_page.text, 'html.parser')
-img_url = soup.find('img')['src']
-
-# Making a GET request to the image URL and save the image
-img_data = session.get(img_url).content
-with open('image.jpg', 'wb') as f:
-    f.write(img_data)
-
-# Display the image using PIL
-img = Image.open('image.jpg')
-img.show()

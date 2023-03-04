dce5ed225e5eb8c18b0e7fb79714fafe6ad6244f
Vanshagra Rai <vanshagrarai@gmail.com>
2023-03-04 19:13:33 +0530

    Added android cam facility

--- cctv.py
+++ cctv.py
@@ -11,7 +11,7 @@ def get_image(url):
     img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
     img = cv2.imdecode(img_arr, -1)
     img = imutils.resize(img, width=1000, height=1800)
-    
+
     # Storing the image in Caputres Folder as Android_cam.jpg
     cv2.imwrite("EatEasy-smart-mess/captures/Android_cam.jpg", img)
 
